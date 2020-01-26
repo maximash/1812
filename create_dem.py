7a590bd22f90f0de1b10640efe44ea2ba00b1f47
maximash <cumpanici.maxim@gmail.com>
2020-01-26 19:30:59 +0200

    added pixels

--- create_dem.py
+++ create_dem.py
@@ -8,16 +8,18 @@ from pyproj import Geod
 
 llcrnrlon = 27.0 # lower left corner longitude, decimal degrees
 llcrnrlat = 45.0 # lower left corner latitude, decimal degrees
-urcrnrlon = 30.0 # upper right corner longitude, decimal degrees
+urcrnrlon = 34.0 # upper right corner longitude, decimal degrees
 urcrnrlat = 48.0 # upper right corner latitude, decimal degrees
 pixel = 100 # size of the pixels, meters
 
 g = Geod(ellps='WGS84') # Use Clarke WGS84 ellipsoid
 az12, az21, width = g.inv(llcrnrlon,llcrnrlat,urcrnrlon,llcrnrlat) # X axis of the map
-az12, az21, length = g.inv(llcrnrlon,llcrnrlat,llcrnrlon,urcrnrlat) # X axis of the map
+az12, az21, length = g.inv(llcrnrlon,llcrnrlat,llcrnrlon,urcrnrlat) # Y axis of the map
 
-print (width /1000)
-print (length/ 1000)
+longitude_pixels = width / pixels # amount of pixel on X axis
+latitude_pixels = length /pixels
+print (longitude_pixels)
+print (latitude_pixels)
 #el = srtm.get_data()
 #elevation = el.get_elevation(latitude,longitude)
 
